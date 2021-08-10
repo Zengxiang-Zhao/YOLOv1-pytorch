ba051dc63573e1c7b03c940504bb467d6fd77f3a
Zengxiang Zhao <zhaozx0308@gmail.com>
2021-08-09 21:15:40 -0400

    using resnet50 as backbone, now can train and test

---
+++ models/model.py
@@ -0,0 +1,79 @@
+from torchvision import  models
+import torch.nn as nn
+from collections import OrderedDict
+import torch
+
+def build_model(anchors,nc,img_size):
+    resnet50 = models.resnet50(pretrained=True)
+    model_backbone = nn.Sequential(
+        OrderedDict([
+                     ('conv1', resnet50.conv1),
+                     ('bn1', resnet50.bn1),
+                     ('relu', resnet50.relu),
+                     ('maxpool',resnet50.maxpool),
+                     ('layer1', resnet50.layer1),
+                     ('layer2', resnet50.layer2),
+                     ('layer3', resnet50.layer3),
+                     ('layer4',resnet50.layer4),
+        ]))
+    model_neck = nn.Sequential(nn.Conv2d(2048,255,kernel_size=3,stride=1,padding=1))
+    model_detector = Detector(anchors,nc,img_size=img_size)
+    model = nn.Sequential(OrderedDict([
+         ('backbone', model_backbone),
+         ('neck', model_neck),
+         ('detector', model_detector)                          
+        ]))
+    return model
+
+
+
+class Detector(nn.Module):
+    def __init__(self, anchors, nc, img_size=416):
+        super(Detector, self).__init__()
+
+        self.anchors = torch.Tensor(anchors)
+        self.na = len(anchors)  # number of anchors (3)
+        self.nc = nc  # number of classes (80)
+        self.nx = 0  # initialize number of x gridpoints
+        self.ny = 0  # initialize number of y gridpoints
+        self.img_size = img_size
+
+    def forward(self, p):
+
+        bs, ny, nx = p.shape[0], p.shape[-2], p.shape[-1]
+        if (self.ny, self.nx) != (ny, nx):
+            create_grids(self, self.img_size, (ny, nx), p.device)
+
+        # p.view(bs, 255, 13, 13) -- > (bs, 3, 13, 13, 85)  # (bs, anchors, grid, grid, classes + xywh)
+        p = p.view(bs, self.na, self.nc + 5, self.ny, self.nx).permute(0, 1, 3, 4, 2).contiguous()  # prediction
+
+        if self.training:
+            return p
+
+        else:  # inference
+            io = p.clone()  # inference output
+            io[..., 0:2] = torch.sigmoid(io[..., 0:2]) + self.grid_xy  # xy
+            io[..., 2:4] = torch.exp(io[..., 2:4]) * self.anchor_wh  # wh yolo method
+            # io[..., 2:4] = ((torch.sigmoid(io[..., 2:4]) * 2) ** 3) * self.anchor_wh  # wh power method
+            io[..., 4:] = torch.sigmoid(io[..., 4:])  # p_conf, p_cls
+            # io[..., 5:] = F.softmax(io[..., 5:], dim=4)  # p_cls
+            io[..., :4] *= self.stride
+
+            # reshape from [1, 3, 13, 13, 85] to [1, 507, 85]
+            return io.view(bs, -1, 5 + self.nc), p
+
+def create_grids(self, img_size, ng, device='cpu'):
+    ny, nx = ng  # x and y grid size
+    self.img_size = img_size
+    self.stride = img_size / max(ng)
+
+    # build xy offsets
+    yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
+    self.grid_xy = torch.stack((xv, yv), 2).to(device).float().view((1, 1, ny, nx, 2))
+
+    # build wh gains
+    self.anchor_vec = self.anchors.to(device) / self.stride
+    self.anchor_wh = self.anchor_vec.view(1, self.na, 1, 1, 2).to(device)
+    self.ng = torch.Tensor(ng).to(device)
+    self.nx = nx
+    self.ny = ny
---
+++ train.py
@@ -0,0 +1,213 @@
+import argparse
+import time
+
+import torch.distributed as dist
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+import test  # Import test.py to get mAP after each epoch
+from models.model import build_model
+from utils.datasets import *
+from utils.utils import *
+
+# Hyperparameters
+# 0.861      0.956      0.936      0.897       1.51      10.39     0.1367    0.01057    0.01181     0.8409     0.1287   0.001028     -3.441     0.9127  0.0004841
+hyp = {'k': 10.39,  # loss multiple
+       'xy': 0.1367,  # xy loss fraction
+       'wh': 0.01057,  # wh loss fraction
+       'cls': 0.01181,  # cls loss fraction
+       'conf': 0.8409,  # conf loss fraction
+       'iou_t': 0.1287,  # iou target-anchor training threshold
+       'lr0': 0.001028,  # initial learning rate
+       'lrf': -3.441,  # final learning rate = lr0 * (10 ** lrf)
+       'momentum': 0.9127,  # SGD momentum
+       'weight_decay': 0.0004841,  # optimizer weight decay
+       }
+
+
+def train(
+        train_imgs_path,
+        train_labels_path,
+        img_size=416,
+        epochs=273,  # 500200 batches at bs 64, dataset length 117263
+        batch_size=16,
+        transfer = False,
+        debug = False,
+        names = None,
+        test_imgs_path=None,
+        test_labels_path=None,
+        resume = False,
+        model = None,
+        save = False,
+):
+
+    class_names = load_classes(names)
+    nc = len(class_names)
+    anchors = [[116, 90], [156, 198], [373, 326]]
+
+    cuda = torch.cuda.is_available()
+    device = torch.device('cuda:0' if cuda else 'cpu')
+
+    # Initialize model
+    if model == None:
+        model = build_model(anchors,nc,img_size).to(device)
+    else:
+        model = model.to(device)
+
+    # Optimizer
+    # optimizer = optim.SGD(model.parameters(), lr=hyp['lr0'], momentum=hyp['momentum'], weight_decay=hyp['weight_decay'])
+    optimizer = optim.Adam(model.parameters(),lr = hyp['lr0'])
+
+    cutoff = -1  # backbone reaches to cutoff layer
+    start_epoch = 0
+    best_loss = float('inf')
+    nf = 255  # yolo layer size (i.e. 255)
+
+
+
+    # Scheduler https://github.com/ultralytics/yolov3/issues/238
+
+    # lf = lambda x: 1 - 10 ** (hyp['lrf'] * (1 - x / epochs))  # inverse exp ramp
+    lf = lambda epoch: 0.65 ** epoch
+
+    scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lf, last_epoch=start_epoch - 1)
+
+    # Dataset for train
+    train_dataset = LoadImagesAndLabels(train_imgs_path, train_labels_path, img_size=img_size, augment=True, debug=debug)
+
+    print(f'There are totally {len(train_dataset)} images to process!')
+
+    # Dataloader for train
+    train_dataloader = DataLoader(train_dataset,
+                            batch_size=batch_size,
+                            # num_workers=opt.num_workers,
+                            shuffle=True,
+                            pin_memory=True,
+                            collate_fn=train_dataset.collate_fn)
+
+    # for test 
+    test_dataset = LoadImagesAndLabels(test_imgs_path,test_labels_path, img_size=img_size, debug=debug)
+    test_dataloader = DataLoader(test_dataset,
+                            batch_size=batch_size,
+                            # num_workers=4,
+                            pin_memory=True,
+                            collate_fn=test_dataset.collate_fn)
+
+    # Start training
+    t = time.time()
+    model.hyp = hyp  # attach hyperparameters to model
+    # model_info(model)
+
+    nb = len(train_dataloader)
+    results = (0, 0, 0, 0, 0)  # P, R, mAP, F1, test_loss
+
+    print(f'There are totally **{len(train_dataset)}** images to process for training!')
+    print(f'There are totally **{len(test_dataset)}** images to process for testing!')
+
+    for epoch in range(start_epoch, epochs):
+        model.train()
+        print(('\n%8s%12s' + '%10s' * 7) % ('Epoch', 'Batch', 'xy', 'wh', 'conf', 'cls', 'total', 'nTargets', 'time'))
+
+        mloss = torch.zeros(5).to(device)  # mean losses
+        for i, (imgs, targets, _, _) in enumerate(train_dataloader):
+            imgs = imgs.to(device)
+            targets = targets.to(device)
+            nt = len(targets)
+
+            # Run model
+            pred = model(imgs)
+
+            optimizer.zero_grad()
+            # Compute loss
+            loss, loss_items = compute_loss(pred, targets, model)
+            if torch.isnan(loss):
+                print('WARNING: nan loss detected, ending training')
+                return results
+
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            # Update running mean of tracked metrics
+            mloss = (mloss * i + loss_items) / (i + 1)
+
+            # Print batch results
+            s = ('%8s%12s' + '%10.3g' * 7) % (
+                '%g/%g' % (epoch, epochs - 1),
+                '%g/%g' % (i, nb - 1), *mloss, nt, time.time() - t)
+            t = time.time()
+            print(s)
+
+        with torch.no_grad():
+            print('\n')
+            results = test.test(names = names, batch_size=batch_size, img_size=img_size, model=model,
+                                conf_thres=0.1, dataloader=test_dataloader)
+        # Write epoch results
+        with open('results.txt', 'a') as file:
+            file.write(s + '%11.3g' * 5 % results + '\n')  # P, R, mAP, F1, test_loss
+
+        # Update best loss
+        test_loss = results[4]
+        if test_loss < best_loss:
+            best_loss = test_loss
+
+        # Save training results
+        save = False
+        if save:
+            # Create checkpoint
+            chkpt = {'epoch': epoch,
+                     'best_loss': best_loss,
+                     'model': model.module.state_dict() if type(
+                         model) is nn.parallel.DistributedDataParallel else model.state_dict(),
+                     'optimizer': optimizer.state_dict()}
+
+            # Save latest checkpoint
+            torch.save(chkpt, latest)
+
+            # Save best checkpoint
+            if best_loss == test_loss:
+                torch.save(chkpt, best)
+
+            # # Save backup every 10 epochs (optional)
+            # if epoch > 0 and epoch % 10 == 0:
+            #     torch.save(chkpt, weights + 'backup%g.pt' % epoch)
+
+            # Delete checkpoint
+            del chkpt
+
+    return results
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=273, help='number of epochs')
+    parser.add_argument('--batch-size', type=int, default=16, help='size of each image batch')
+    parser.add_argument('--names', type=str, help='file conatin object names')
+    parser.add_argument('--img-size', type=int, default=416, help='pixels')
+    parser.add_argument('--train_imgs_path', type=str, help='folder contain images')
+    parser.add_argument('--train_labels_path', type=str, help='folder contain labels')
+    parser.add_argument('--test_imgs_path', type=str, help='folder contain images')
+    parser.add_argument('--test_labels_path', type=str, help='folder contain labels')
+    parser.add_argument('--transfer', type = int,default=0, help='Whether only train the yolo layers: 0 False, 1 True')
+    parser.add_argument('--debug', type=int,default=0, help='if Ture only use two images: 0 False, 1 True')
+    parser.add_argument('--resume', type=int,default=0, help='if Ture to resume: 0 False, 1 True')
+    opt = parser.parse_args()
+    print(opt, end='\n\n')
+
+    # Train
+    results = train(
+        train_imgs_path = opt.train_imgs_path,
+        train_labels_path = opt.train_labels_path,
+        test_imgs_path = opt.test_imgs_path,
+        test_labels_path = opt.test_labels_path,
+        img_size=opt.img_size,
+        epochs=opt.epochs,
+        batch_size=opt.batch_size,
+        transfer= True if opt.transfer else False,
+        debug = True if opt.debug else False,
+        names = opt.names,
+        resume = True if opt.resume else False,
+    )
+
+
